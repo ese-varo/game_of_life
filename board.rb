b2c5503e0d9ff4efa9f75796af4c92639044c780
Alvaro <varoboss.ap@gmail.com>
2020-01-24 11:34:19 -0600

    Create fill and print_matrix methods on board class

--- board.rb
+++ board.rb
@@ -6,6 +6,24 @@ class Board
     @matrix = []
   end
 
+  def fill
+    size.times do |x|
+      matrix.push([])
+      size.times { |y| matrix[x][y] = Cell.new }
+    end
+  end
+
+  def print_matrix
+    matrix.each do |row|
+      row.each { |cell| print cell.symbol }
+      print "\n"
+    end
+  end
+
+  def random_selection
+    
+  end
+
   private 
 
   attr_writer :matrix
