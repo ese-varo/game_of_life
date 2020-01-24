0759c46a8cce93d1b503be6927fbdc009c5dbb43
Alvaro <varoboss.ap@gmail.com>
2020-01-24 10:59:58 -0600

    Add class methods to cell

--- cell.rb
+++ cell.rb
@@ -1,6 +1,25 @@
 class Cell
+  attr_accessor :new_state
+  attr_reader :symbol
+
   def initialize
-    @symbol = {alive: "@", dead: " "}
-    @state = {current: false, new: false}
+    @symbols = {alive: "@", dead: "-"}
+    @alive = false
+    @new_state = false
+    @symbol = "-" 
   end
+
+  def alive=(value)
+    @current_state = value
+    current_state ? symbol = symbols[:alive] : symbol = symbols[:dead]
+  end
+
+  def alive?
+    @alive
+  end
+
+  private
+
+  attr_reader :symbols
+  attr_writer :symbol
 end
